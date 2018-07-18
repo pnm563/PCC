804679ee32202386a80000314b2fca2d040211bd
pmattingly <paul.mattingly@niu-solutions.com>
2018-07-18 14:19:57 +0100

    Parameter and Parameter value FKs fixed and added

---
+++ CSIPricingConfiguratorDatabase/dbo/Foreign Keys/FK_ParameterValue_ParameterID_PK_Parameter_ID.sql
@@ -0,0 +1,5 @@
+﻿ALTER TABLE [dbo].ParameterValue
+	ADD CONSTRAINT [FK_ParameterValue_ParameterID_PK_Parameter_ID]
+	FOREIGN KEY (ParameterID)
+	REFERENCES [Parameter] (ID)
+	ON DELETE CASCADE
--- CSIPricingConfiguratorDatabase/dbo/Tables/ParameterValue.sql
+++ CSIPricingConfiguratorDatabase/dbo/Tables/ParameterValue.sql
@@ -1,6 +1,8 @@
 ﻿CREATE TABLE [dbo].[ParameterValue] (
     [ParameterID] UNIQUEIDENTIFIER NOT NULL,
     [Label]       NVARCHAR (255)   NOT NULL,
-    [Value]       NVARCHAR (255)   NOT NULL
+    [Value]       NVARCHAR (255)   NOT NULL, 
+    [ID] UNIQUEIDENTIFIER NOT NULL DEFAULT newid(), 
+    CONSTRAINT [PK_ParameterValue] PRIMARY KEY ([ID])
 );
 
