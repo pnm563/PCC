d3b9a36ba488d8fc80c732aea513b5dc4d55f926
Paul Mattingly <paul.mattingly@niu-solutions.com>
2018-10-17 13:07:52 +0100

    Ready to do typeAhead testing

--- CSIProductConfigurationBackend/Controllers/CustomerController.cs
+++ CSIProductConfigurationBackend/Controllers/CustomerController.cs
@@ -4,10 +4,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 
 namespace CSIProductConfigurationBackend.Controllers
 {
-    public class CustomerController
+    public class CustomerController : ApiController
     {
         public List<Customer> Get()
         {
