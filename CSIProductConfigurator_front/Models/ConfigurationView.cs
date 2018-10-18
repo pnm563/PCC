cc2bfffd177dd4fe4f5ad1a64fc978c7da94a095
Paul Mattingly <paul.mattingly@niu-solutions.com>
2018-10-18 13:08:46 +0100

    Validation sorted for Customer Code

--- CSIProductConfigurator_front/Models/ConfigurationView.cs
+++ CSIProductConfigurator_front/Models/ConfigurationView.cs
@@ -12,13 +12,15 @@ namespace CSIProductConfigurator_front.Models
     {
         [Required]
         public String Name { get; set; }
+        [Required]
         public String Description { get; set; }
+        [Required]
         public String CustomerName { get; set; }
 
         //public List<String> CustomerCodes { get; set; } using typeAhead search instead
         public List<ConfigurationType> ConfigurationTypes { get; set; }
 
-        [Display(Name = "Customer Code")]
+        [Display(Name = "Customer Code"),Required(ErrorMessage = "The Customer Code is required. Please select a customer to obtain code.")]
         public String SelectedCustomerCode { get; set; }
         [Display(Name = "Configuration Type")]
         public String SelectedConfigurationType { get; set; }
