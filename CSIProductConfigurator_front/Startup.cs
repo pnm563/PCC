2f39acf9e2ba71e7b14b432285a353822496cf5f
Paul Mattingly <paul.mattingly@niu-solutions.com>
2018-10-24 15:44:46 +0100

    ASP Identity in!

---
+++ CSIProductConfigurator_front/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CSIProductConfigurator_front.Startup))]
+namespace CSIProductConfigurator_front
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
