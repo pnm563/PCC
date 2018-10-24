f980c6b805afdf8eaf678d911adab6d113e8e225
Paul Mattingly <paul.mattingly@niu-solutions.com>
2018-10-24 15:00:28 +0100

    ASPIdentity project added

---
+++ ASPIdentityExample/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ASPIdentityExample.Startup))]
+namespace ASPIdentityExample
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
