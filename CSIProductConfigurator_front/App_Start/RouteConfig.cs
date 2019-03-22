a7d4a1a308e19a0d86c61dc9e1ac815348d5bfe4
Paul Mattingly <paul.mattingly@niu-solutions.com>
2019-03-22 09:07:27 +0000

    Seems to be working after AntiForgeryClaims fix

--- CSIProductConfigurator_front/App_Start/RouteConfig.cs
+++ CSIProductConfigurator_front/App_Start/RouteConfig.cs
@@ -16,8 +16,11 @@ namespace CSIProductConfigurator_front
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Account", action = "Login", id = UrlParameter.Optional }
+                //defaults: new { controller = "Account", action = "Login", id = UrlParameter.Optional }
+                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                 namespaces: new string[] { "CSIProductConfigurator_front.Controllers" }
             );
+            
         }
     }
 }
